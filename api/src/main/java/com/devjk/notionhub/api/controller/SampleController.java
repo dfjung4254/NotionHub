169752935e3f756fb861312e284fafa46413a9d9
dfjung4254 <rmsghk4254@naver.com>
2021-05-16 01:30:40 +0900

    application.yml : 로그레벨 추가 및 CORS 설정

--- api/src/main/java/com/devjk/notionhub/api/controller/SampleController.java
+++ api/src/main/java/com/devjk/notionhub/api/controller/SampleController.java
@@ -4,13 +4,10 @@ import com.devjk.notionhub.api.annotation.NotionHubRestController;
 import com.devjk.notionhub.api.model.Sample;
 import com.devjk.notionhub.api.service.SampleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @NotionHubRestController
 public class SampleController {
