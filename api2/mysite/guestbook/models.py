26d1f8475dcb8b8fb9e1a3662163a5aa33112e5b
thals7 <thals_7@naver.com>
2021-06-12 20:20:12 +0900

    models.py class name 수정

--- api2/mysite/guestbook/models.py
+++ api2/mysite/guestbook/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Post(models.Model):
+class guestbook(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField()
 
--- api2/mysite/guestbook/views.py
+++ api2/mysite/guestbook/views.py
@@ -2,21 +2,25 @@ import json
 from django.views import View
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers # 모델(쿼리셋)을 JSON 타입으로 데이터 직렬화
-from .models import Post
+from .models import guestbook
+
+# http 통신 : resful api
+# uri : http://localhost/guestbook
+# 메소드 : get, post, put, delete
 
 
 class index(View):
     def get(self, request):
-        data = serializers.serialize('json',Post.objects.all())
+        data = serializers.serialize('json',guestbook.objects.all())
         return HttpResponse(data, content_type="application/json")
 
     def post(self, request):
         if request.META['CONTENT_TYPE'] == "application/json":
             request = json.loads(request.body)
-            data = Post(title = request['title'],
+            data = guestbook(title = request['title'],
                         content = request['content'])
         else:
-            data = Post(title = request.POST['title'],
+            data = guestbook(title = request.POST['title'],
                         content = request.POST['content'])
         data.save()
         return HttpResponse("저장되었습니다.")
@@ -25,7 +29,7 @@ class index(View):
 class detail(View):
     def get(self,request,id):
         if id == 1:
-            data = serializers.serialize('json',Post.objects.filter(id=1))
+            data = serializers.serialize('json',guestbook.objects.filter(id=1))
             return HttpResponse(data, content_type="application/json")
         else:
             return HttpResponse("Error: 유효하지 않은 접근입니다.")
